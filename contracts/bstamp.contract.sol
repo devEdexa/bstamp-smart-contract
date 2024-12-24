1f76618c6840438d77d4bf92adc3a3f830cbfe8a
nish <n.ambalapatta@edexa.team>
2024-12-24 12:14:23 +0530

    original contract restored

--- contracts/bstamp.contract.sol
+++ contracts/bstamp.contract.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
-import '@openzeppelin/contracts/security/Pausable.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/utils/Counters.sol';
+import '@openzeppelin/contracts@4.5.0/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts@4.5.0/token/ERC721/extensions/ERC721URIStorage.sol';
+import '@openzeppelin/contracts@4.5.0/security/Pausable.sol';
+import '@openzeppelin/contracts@4.5.0/access/Ownable.sol';
+import '@openzeppelin/contracts@4.5.0/utils/Counters.sol';
 
 contract BSTAMP is Pausable, Ownable {
     struct Bstamp {
